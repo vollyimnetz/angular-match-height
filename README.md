38739c88cc51623b853f00c04d8b8b799fb63972
Simon Staton <simon.staton@live.co.uk>
2016-06-22 11:06:04 +0100

    Adding angular-same-height source code from original npm module

---
+++ README.md
@@ -0,0 +1,50 @@
+Match height directive
+=====================
+
+[All Credits Go To eatinabit - https://www.npmjs.com/package/angular-same-height](https://www.npmjs.com/package/angular-same-height)
+An Angular directive that keeps elements at the same height. With updated scope broadcasting.
+
+(This directive does not work in IE <= 8.)
+
+[![Build Status](https://travis-ci.org/amyboyd/angular-same-height-directive.svg?branch=master)](https://travis-ci.org/amyboyd/angular-same-height-directive)
+
+How to install
+--------------
+
+	npm install angular-same-height --save
+
+
+How to use
+----------
+
+Format:
+
+	same-height="MEDIA QUERY { SELECTOR } [, MEDIA QUERY { SELECTOR } ]"
+
+If `MEDIA QUERY` is `*`, the elements found by `SELECTOR` are always kept at the same height.
+
+Examples:
+
+	<article same-height="(min-width: 901px) { h2 } screen and (min-width: 901px) { div p:nth-of-type(1) }">
+		<div>
+			<h1>Title 1</h1>
+			<p>Paragraph 1</p>
+		</div>
+
+		<div>
+			<h1>Title 2 <br /> Title 2</h1>
+			<p>Paragraph 2 <br /> Paragraph 2</p>
+			<p>Paragraph 3 (not kept the same height)</p>
+		</div>
+	</article>
+
+	<ul same-height="* { li }">
+		<li>Text</li>
+		<li>Text<br />Text</li>
+		<li>Text<br />Text<br />Text</li>
+	</ul>
+
+Running the tests
+-----------------
+
+	npm test
---
+++ index.js
@@ -0,0 +1,125 @@
+angular
+  .module('partnermarketing.matchHeight', [])
+  .directive('matchHeight', ['$window', function ($window) {
+
+    'use strict';
+
+    function getElementHeight(element) {
+      return $window.parseInt($window.getComputedStyle(element, null).getPropertyValue('height'), 10);
+    }
+
+    function same(selector, element) {
+      // Ensure we aren't calculating based on any previously forced heights.
+      removeSame(selector, element);
+
+      var greatestHeight = 0,
+        elements = findTargetElements(selector, element);
+
+      for (var i = 0; i < elements.length; i++) {
+        var height = getElementHeight(elements[i]);
+        if (height > greatestHeight) {
+          greatestHeight = height;
+        }
+      }
+
+      var newHeightCss = String(greatestHeight) + 'px'
+      elements.map(function(element) { 
+        element.style.height = newHeightCss;
+      });
+    }
+
+    function removeSame(selector, element) {
+      var elements = findTargetElements(selector, element);
+      for (var i = 0; i < elements.length; i++) {
+        elements[i].style.height = 'auto';
+      }
+    }
+
+    var trimString = function(string) {
+      return string.replace(/^ +/, '').replace(/ +$/, '');
+    };
+
+    var findTargetElements = function(selector, element) {
+      element[0].querySelectorAll(selector).filter(function(el) {
+        return !isHidden(el)
+      });
+    };
+
+    function isHidden(element) {
+      // `offsetParent_` is used by unit tests because `offsetParent` is read-only.
+      if (element.hasOwnProperty('offsetParent_')) {
+        return (element.offsetParent_ === null);
+      } else {
+        return (element.offsetParent === null);
+      }
+    }
+
+    return {
+      restrict: 'A',
+      scope: {
+        matchHeight: '@'
+      },
+      compile: function compile() {
+        return {
+          post: function postLink(scope, element) {
+            if (!angular.isString(scope.matchHeight)) {
+              return;
+            }
+            var targets = scope.matchHeight.split('}');
+            if (targets.length < 2) {
+              return;
+            }
+
+            var mediaQueriesAndTargets = [];
+            for (var i = 0; i < targets.length; i++) {
+              var mediaQuery = trimString(targets[i].replace(/^(.+)\{.+$/, '$1'));
+              var target = trimString(targets[i].replace(/^.+\{(.+)$/, '$1'));
+
+              if (mediaQuery === '' && target === '') {
+                continue;
+              }
+
+              mediaQueriesAndTargets.push([mediaQuery, target]);
+            }
+
+            function checkMediaQuery() {
+              for (var i = 0; i < mediaQueriesAndTargets.length; i++) {
+                var mediaQuery = mediaQueriesAndTargets[i][0];
+                var selector = mediaQueriesAndTargets[i][1];
+
+                if (mediaQuery === '*' ||
+                    (typeof $window.matchMedia !== 'undefined' && $window.matchMedia(mediaQuery))) {
+                  same(selector, element);
+                } else {
+                  removeSame(selector, element);
+                }
+              }
+            }
+
+            var resizeTimer;
+
+            $window.addEventListener('resize', function() {
+              $window.clearTimeout(resizeTimer);
+              resizeTimer = $window.setTimeout(checkMediaQuery, 10);
+            });
+            resizeTimer = $window.setTimeout(checkMediaQuery, 10);
+
+            // Watch for DOM mutation, which could mean elements being inserted or styles
+            // changing, and make sure we recalculate the heights after DOM mutation.
+            var domMutationEvents = [
+              'DOMAttributeNameChanged',
+              'DOMCharacterDataModified',
+              'DOMElementNameChanged',
+              'DOMNodeInserted',
+              'DOMNodeInsertedIntoDocument',
+              'DOMNodeRemoved',
+              'DOMNodeRemovedFromDocument'
+            ];
+            for (var i = 0; i < domMutationEvents.length; i++) {
+              element[0].addEventListener(domMutationEvents[i], checkMediaQuery);
+            }
+          }
+        };
+      }
+    };
+  }]);
